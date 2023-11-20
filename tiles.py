a4550aeff636e93cd58b5deb02dbb5d7e36fc1a1
saperoi <uesugi.sapero@gmail.com>
2023-11-20 15:49:45 +0100

    tiles py file that is only a dictionary

---
+++ tiles.py
@@ -0,0 +1,9 @@
+tiles = {
+    "floor": "assets/tiles/floor.png",
+    "floor1": "assets/tiles/floor1.png",
+    "floor2": "assets/tiles/floor2.png",
+    "frontwall_center": "assets/tiles/frontwall_center.png",
+    "frontwall_left": "assets/tiles/frontwall_left.png",
+    "frontwall_right": "assets/tiles/frontwall_right.png",
+    "stairs_down": "assets/tiles/stairs_down.png",
+}
