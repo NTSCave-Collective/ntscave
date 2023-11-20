49b524a84590ba00f6a328d2c2c1abbba3f4f884
Matthias Van de Casteele <matthias.vandecasteele@student.ucll.be>
2023-11-20 13:52:12 +0100

    screen flickering fixed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pygame
-import player
+import renderer
+import time
 
 global running
 
@@ -13,20 +14,22 @@ def main():
 def create_main_surface():
     # Tuple representing width and height in pixels
     screen_size = (1024, 768)
+    clock = pygame.time.Clock()
     
     running = True
 
+    # Create window with given size
+    window = pygame.display.set_mode(screen_size)
+    window.fill([255,255,255])
+    
     while running:
         pygame.event.pump()
-        # Create window with given size
-        myWindow = pygame.display.set_mode(screen_size)
-        myWindow.fill([255,255,255])
         windowEvent = pygame.event.get()
-        player.drawCircle(myWindow)
         for event in windowEvent:
             if event.type == pygame.QUIT:
                 running = False
-        pygame.display.flip()
+        renderer.render_frame(window)
+        clock.tick(60)  # number of computation steps per second, should be greater equal to FPS-value
 
 if __name__ == "__main__":
     main()
---
+++ renderer.py
@@ -0,0 +1,7 @@
+import pygame
+import player
+
+def render_frame(window):
+    # Drawing current frame
+    player.drawCircle(window)
+    pygame.display.flip()
